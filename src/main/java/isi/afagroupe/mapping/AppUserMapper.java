6fe89dea34bf92ba57317043cc26916163acfe72
Fallou MBOW <118375689+mbowfallou@users.noreply.github.com>
2022-12-06 11:22:39 +0000

    Update AppUserMapper.java

--- src/main/java/isi/afagroupe/mapping/AppUserMapper.java
+++ src/main/java/isi/afagroupe/mapping/AppUserMapper.java
@@ -3,9 +3,11 @@ package isi.afagroupe.mapping;
 import isi.afagroupe.dto.AppUser;
 import isi.afagroupe.entities.AppUserEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface AppUserMapper {
+    AppUserMapper INSTANCE = Mappers.getMapper(AppUserMapper.class);
     AppUserEntity fromAppUser(AppUser appUser);
     AppUser toAppUser(AppUserEntity appUserEntity);
 }
