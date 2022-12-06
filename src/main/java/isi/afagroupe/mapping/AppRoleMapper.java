4c87679386e114127f2e387dc42c6e76238e95c6
Fallou MBOW <118375689+mbowfallou@users.noreply.github.com>
2022-12-06 11:22:12 +0000

    Update AppRoleMapper.java

--- src/main/java/isi/afagroupe/mapping/AppRoleMapper.java
+++ src/main/java/isi/afagroupe/mapping/AppRoleMapper.java
@@ -3,9 +3,12 @@ package isi.afagroupe.mapping;
 import isi.afagroupe.dto.AppRole;
 import isi.afagroupe.entities.AppRoleEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AppRoleMapper {
+    AppRoleMapper INSTANCE = Mappers.getMapper(AppRoleMapper.class);
     AppRoleEntity fromAppRole(AppRole appRole);
     AppRole toAppRole(AppRoleEntity appRoleEntity);
 }
